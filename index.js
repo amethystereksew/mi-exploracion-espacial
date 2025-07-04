f49b41af74d67de2352dc73cc41196b5dfad229c
amethyste <javiahernandez67@gmail.com>
2025-07-03 23:13:03 -0600

    añadir script para ejecutar el reporte de planetas

---
+++ index.js
@@ -0,0 +1,14 @@
+const planetas = require('./planetas');
+
+// Reporte básico
+planetas.forEach(planeta => {
+  console.log(`¡Planeta ${planeta.nombre} descubierto!`);
+  console.log(`Descripción: ${planeta.descripcion}`);
+  console.log(`Descubierto en: ${planeta.descubiertoEn}`);
+  console.log('---');
+});
+
+
+
+
+
