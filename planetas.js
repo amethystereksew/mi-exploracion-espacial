5e580c6eccdd76ac3f6a1675ebe206c371433f0e
amethyste <javiahernandez67@gmail.com>
2025-07-03 23:10:32 -0600

    first commit

---
+++ planetas.js
@@ -0,0 +1,24 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Kepler-452b",
+    descripcion: "Un planeta similar a la Tierra, ubicado a 1.400 años luz.",
+    descubiertoEn: "2015"
+  },
+  {
+    nombre: "Gliese 581g",
+    descripcion: "Un posible planeta habitable en el sistema Gliese 581.",
+    descubiertoEn: "2010"
+  }
+];
+
+module.exports = planetas;
